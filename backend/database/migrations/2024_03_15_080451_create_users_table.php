2b2d328c3493f5d5a4d19144e1725e56e44d4aae
MrIsdi <isdiridho@gmail.com>
2024-03-20 11:42:27 +0700

    update: integrate Auth

--- backend/database/migrations/2024_03_15_080451_create_users_table.php
+++ backend/database/migrations/2024_03_15_080451_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger("role_id");
+            $table->string("phone")->default("");
+            $table->string("address")->default("");
             $table->rememberToken();
             $table->timestamps();
 
