3af4f93c737d06c942d6c8bf4edddb75df9ad7e3
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-03-20 15:16:25 +0700

    style: fix some layouts

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -16,183 +16,138 @@ import { z } from "zod";
 import { signInSchema } from "@/type/auth";
 import { useForm } from "react-hook-form";
 import { useLogin } from "@/stores/useAuth";
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/components/ui/use-toast";
 
 export default function SignIn() {
-  const { message, handleSignIn } = useLogin()
-  const { toast } = useToast()
+  const { message, handleSignIn } = useLogin();
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
   });
   const onSubmit = async (val: z.infer<typeof signInSchema>) => {
     await handleSignIn(val)
-    .then(()=>{
-      toast({
-        title: "Success",
-        description: message
+      .then(() => {
+        toast({
+          title: "Success",
+          description: message,
+        });
       })
-    })
-    .catch(()=>{
-      toast({
-        variant: "destructive",
-        title: "Failed",
-        description: message
-      })
-    })
-  }
+      .catch(() => {
+        toast({
+          variant: "destructive",
+          title: "Failed",
+          description: message,
+        });
+      });
+  };
   return (
-    <>
-      <section className="mt-20 bg-white lg:mt-0 h-[calc(100%-80px)] relative top-20">
-        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-          <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-            <Image
-              height={1000}
-              width={500}
-              alt=""
-              src="/image.png"
-              className="absolute inset-0 h-full w-full object-cover opacity-80"
-            />
+    <section className="relative mt-20 h-full bg-white lg:mt-0">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <Image
+            height={1000}
+            width={500}
+            alt=""
+            src="/image.png"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
 
-            <div className="hidden lg:relative lg:block lg:p-12">
-              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Welcome to Adadeh
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+              Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
+              mulai dari sepatu olahraga, dan sneaker, hingga aksesori olahraga
+              lainnya untuk semua kebutuhan Anda.
+            </p>
+          </div>
+        </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <a
+                className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+                href="#"
+              >
+                <span className="sr-only">Home</span>
+                <Image src="/adadeh.svg" alt="Logo" width={50} height={50} />
+              </a>
+
+              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                 Welcome to Adadeh
-              </h2>
+              </h1>
 
-              <p className="mt-4 leading-relaxed text-white/90">
+              <p className="mt-4 leading-relaxed text-gray-500">
                 Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
                 mulai dari sepatu olahraga, dan sneaker, hingga aksesori
                 olahraga lainnya untuk semua kebutuhan Anda.
               </p>
             </div>
-          </section>
 
-          <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-            <div className="max-w-xl lg:max-w-3xl">
-              <div className="relative -mt-16 block lg:hidden">
-                <a
-                  className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
-                  href="#"
+            <div className="flex flex-col border-gray-300 px-8 py-14 lg:border">
+              <h1 className="hidden text-4xl font-bold lg:block">
+                WELCOME BACK.
+              </h1>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4 lg:mt-8 lg:w-96"
+                  autoComplete="false"
                 >
-                  <span className="sr-only">Home</span>
-                  <Image src="/adadeh.svg" alt="Logo" width={50} height={50} />
-                </a>
-
-                <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                  Welcome to Adadeh
-                </h1>
-
-                <p className="mt-4 leading-relaxed text-gray-500">
-                  Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
-                  mulai dari sepatu olahraga, dan sneaker, hingga aksesori
-                  olahraga lainnya untuk semua kebutuhan Anda.
-                </p>
-              </div>
-
-              <div className="flex flex-col border-gray-300 px-8 py-14 lg:border">
-                <h1 className="hidden text-4xl font-bold lg:block">
-                  WELCOME BACK.
-                </h1>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-4 lg:mt-8 lg:w-96"
-                    autoComplete="false"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Email</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="email"
-                              placeholder="Email"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password</FormLabel>
-                          <FormControl>
-                            <Input type="password" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <Button type="submit">Submit</Button>
-                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-                      Dont have an account?
-                      <a href="/auth/signup" className="text-gray-700 underline">
-                        Sign Up
-                      </a>
-                    </p>
-                  </form>
-                </Form>
-                {/* <form action="#" className="flex flex-col lg:mt-8 lg:w-96">
-                  <div className="mt-2">
-                    <label
-                      htmlFor="Email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Email{" "}
-                    </label>
-
-                    <input
-                      placeholder="john.doe@example.com"
-                      type="email"
-                      id="Email"
-                      name="email"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-
-                  <div className="mt-2">
-                    <label
-                      htmlFor="Password"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Password{" "}
-                    </label>
-
-                    <input
-                      type="password"
-                      placeholder="password"
-                      id="Password"
-                      name="password"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-
-                  <div className="mt-4">
-                    <button className="mb-4 inline-block shrink-0 border border-black bg-black px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-black">
-                      LOGIN
-                    </button>
-
-                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-                      Dont have an account?
-                      <a href="#" className="text-gray-700 underline">
-                        Sign Up
-                      </a>
-                      .
-                    </p>
-                  </div>
-                </form> */}
-              </div>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="rounded-none"
+                            type="email"
+                            placeholder="Email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="rounded-none"
+                            type="password"
+                            placeholder="******"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="rounded-none">
+                    Submit
+                  </Button>
+                  <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                    Dont have an account?{" "}
+                    <a href="/auth/signup" className="text-gray-700 underline">
+                      Sign Up
+                    </a>
+                  </p>
+                </form>
+              </Form>
             </div>
-          </main>
-        </div>
-      </section>
-    </>
+          </div>
+        </main>
+      </div>
+    </section>
   );
 }
--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -49,226 +49,151 @@ export default function SignUp() {
       });
   };
   return (
-    <>
-      <section className="relative top-20 mt-20 h-[calc(100%-80px)] bg-white lg:mt-0">
-        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-          <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-            <Image
-              height={1000}
-              width={500}
-              alt=""
-              src="/signup.png"
-              className="absolute inset-0 h-full w-full object-cover opacity-80"
-            />
+    <section className="relative mt-20 h-[calc(100%-80px)] bg-white lg:mt-0">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <Image
+            height={1000}
+            width={500}
+            alt=""
+            src="/signup.png"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
 
-            <div className="hidden lg:relative lg:block lg:p-12">
-              <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Welcome to Adadeh
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+              Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
+              mulai dari sepatu olahraga, dan sneaker, hingga aksesori olahraga
+              lainnya untuk semua kebutuhan Anda.
+            </p>
+          </div>
+        </section>
+
+        <main className="mt-20 flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <a
+                className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+                href="#"
+              >
+                <span className="sr-only">Home</span>
+                <Image src="/adadeh.svg" alt="Logo" width={50} height={50} />
+              </a>
+
+              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                 Welcome to Adadeh
-              </h2>
+              </h1>
 
-              <p className="mt-4 leading-relaxed text-white/90">
+              <p className="mt-4 leading-relaxed text-gray-500">
                 Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
                 mulai dari sepatu olahraga, dan sneaker, hingga aksesori
                 olahraga lainnya untuk semua kebutuhan Anda.
               </p>
             </div>
-          </section>
 
-          <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-            <div className="max-w-xl lg:max-w-3xl">
-              <div className="relative -mt-16 block lg:hidden">
-                <a
-                  className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
-                  href="#"
+            <div className="flex flex-col justify-center border-gray-300 py-14 lg:border lg:px-8">
+              <h1 className="hidden text-4xl font-bold lg:block">SIGN UP.</h1>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4 lg:mt-8 lg:w-96"
+                  autoComplete="false"
                 >
-                  <span className="sr-only">Home</span>
-                  <Image src="/adadeh.svg" alt="Logo" width={50} height={50} />
-                </a>
-
-                <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                  Welcome to Adadeh
-                </h1>
-
-                <p className="mt-4 leading-relaxed text-gray-500">
-                  Toko Online Resmi Adadeh Indonesia menyediakan produk terbaik
-                  mulai dari sepatu olahraga, dan sneaker, hingga aksesori
-                  olahraga lainnya untuk semua kebutuhan Anda.
-                </p>
-              </div>
-
-              <div className="flex flex-col border-gray-300 px-8 py-14 lg:border">
-                <h1 className="hidden text-4xl font-bold lg:block">SIGN UP.</h1>
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-4 lg:mt-8 lg:w-96"
-                    autoComplete="false"
-                  >
-                    {validasi && (
-                      <p className="text-sm text-red-500">
-                        Konfirmasi password harus sama dengan password
-                      </p>
+                  {validasi && (
+                    <p className="text-sm text-red-500">
+                      Konfirmasi password harus sama dengan password
+                    </p>
+                  )}
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="rounded-none"
+                            placeholder="Name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
                     )}
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Name</FormLabel>
-                          <FormControl>
-                            <Input placeholder="name" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Email</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="email"
-                              placeholder="Email"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password</FormLabel>
-                          <FormControl>
-                            <Input type="password" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="password_confirmation"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password Confirmation</FormLabel>
-                          <FormControl>
-                            <Input type="password" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <Button type="submit">Submit</Button>
-                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-                      Already have an account?
-                      <a
-                        href="/auth/signin"
-                        className="text-gray-700 underline"
-                      >
-                        Login.
-                      </a>
-                    </p>
-                  </form>
-                </Form>
-                {/* <form
-                  action="#"
-                  className="flex flex-col gap-6 lg:mt-8 lg:w-96"
-                >
-                  <div>
-                    <label
-                      htmlFor="Name"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Name{" "}
-                    </label>
-
-                    <input
-                      placeholder="John Doe"
-                      type="text"
-                      id="Name"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="Email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Email{" "}
-                    </label>
-
-                    <input
-                      placeholder="john.doe@example.com"
-                      type="email"
-                      id="Email"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="Password"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Password{" "}
-                    </label>
-
-                    <input
-                      type="password"
-                      placeholder="password"
-                      id="Password"
-                      name="password"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="PasswordC"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Password Confirmation{" "}
-                    </label>
-
-                    <input
-                      type="password"
-                      placeholder="password"
-                      id="PasswordC"
-                      name="password_confirmation"
-                      className="mt-1 w-full border border-gray-300 bg-white p-2 text-sm text-gray-700"
-                    />
-                  </div>
-
-                  <div>
-                    <button className="mb-4 inline-block shrink-0 border border-black bg-black px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-black">
-                      SIGN UP
-                    </button>
-
-                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-                      Already have an account?
-                      <a href="#" className="text-gray-700 underline">
-                        Login.
-                      </a>
-                      .
-                    </p>
-                  </div>
-                </form> */}
-              </div>
+                  />
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="rounded-none"
+                            type="email"
+                            placeholder="Email"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            className="rounded-none"
+                            placeholder="******"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password_confirmation"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password Confirmation</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            className="rounded-none"
+                            placeholder="******"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="rounded-none">
+                    Submit
+                  </Button>
+                  <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                    Already have an account?{" "}
+                    <a href="/auth/signin" className="text-gray-700 underline">
+                      Login.
+                    </a>
+                  </p>
+                </form>
+              </Form>
             </div>
-          </main>
-        </div>
-      </section>
-    </>
+          </div>
+        </main>
+      </div>
+    </section>
   );
 }
