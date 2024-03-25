047438d699248adc7cff1ea44d302aa6f1f4fa30
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-03-25 11:09:01 +0700

    fix: fix protected routes

--- frontend/src/components/layouts/ProtectedRoutes.tsx
+++ frontend/src/components/layouts/ProtectedRoutes.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { loginRoutes, protectedRoutes } from "@/lib/constants";
-import { useLogin } from "@/stores/useAuth";
+import { useLogin, useUserLoading } from "@/stores/useAuth";
 import { usePathname, redirect } from "next/navigation";
 import { useEffect } from "react";
 
@@ -11,10 +11,15 @@ export default function ProtectedRoutes({
   children: React.ReactNode;
 }) {
   const { status, data } = useLogin();
+  const { isLoading } = useUserLoading();
   const path = usePathname();
 
   useEffect(() => {
-    if (status) {
+    if (!isLoading && !status) {
+      if (protectedRoutes.includes(path)) {
+        redirect("/auth/signin");
+      }
+    } else if (!isLoading && status) {
       if (loginRoutes.includes(path)) {
         if (data.user.role == "admin") {
           redirect("/dashboard");
@@ -22,11 +27,21 @@ export default function ProtectedRoutes({
           redirect("/user");
         }
       }
-    } else {
-      if (protectedRoutes.includes(path)) {
-        redirect("/auth/signin");
-      }
     }
+
+    // if (status) {
+    //   if (loginRoutes.includes(path)) {
+    //     if (data.user.role == "admin") {
+    //       redirect("/dashboard");
+    //     } else {
+    //       redirect("/user");
+    //     }
+    //   }
+    // } else {
+    //   if (protectedRoutes.includes(path)) {
+    //     redirect("/auth/signin");
+    //   }
+    // }
   }, [path, status, data]);
 
   return <>{children}</>;
