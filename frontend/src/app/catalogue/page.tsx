8db1a5af887a7b607276fa71dcdf7ee9b3e93361
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-03-26 11:40:47 +0700

    fix: catalogue

--- frontend/src/app/catalogue/page.tsx
+++ frontend/src/app/catalogue/page.tsx
@@ -2,12 +2,24 @@
 
 import CatalogFilter from "@/components/CatalogFilter";
 import CatalogList from "@/components/CatalogList";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+// function Search({ children }: { children: React.ReactNode }) {
+//   const searchParams = useSearchParams();
+//   const searchCat = searchParams.get("category") ?? null;
+//   const searchName = searchParams.get("name") ?? null;
+
+//   return <>{children}</>;
+// }
 
 export default function CatalogPage() {
   return (
     <main className="flex min-h-screen flex-col items-center py-24">
       <div className="flex w-full flex-col items-center justify-center gap-4 px-4 md:flex-row md:items-start">
-        <CatalogFilter className="w-full md:basis-1/4" />
+        <Suspense>
+          <CatalogFilter className="w-full md:basis-1/4" />
+        </Suspense>
         <CatalogList className="w-full md:basis-3/4" />
       </div>
     </main>
