d7d11d7fe4d6a6e493cf75e750acc3eb3729fc2c
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-03-27 00:37:12 +0700

    fix: fix possible null value

--- frontend/src/app/dashboard/payments/columns.tsx
+++ frontend/src/app/dashboard/payments/columns.tsx
@@ -14,11 +14,11 @@ import { useLogin } from "@/stores/useAuth";
 
 export type Category = {
   id: string;
-  order_id: string
-  payment_type: string
-  customer: string
-  jumlah: string
-  status: string
+  order_id: string;
+  payment_type: string;
+  customer: string;
+  jumlah: string;
+  status: string;
   action: string;
 };
 
@@ -93,41 +93,58 @@ export const columns: ColumnDef<Category>[] = [
     accessorKey: "action",
     header: "Action",
     cell: ({ row }) => {
-      const payment = row.original
-      const { data } = useLogin()
-      const router = useRouter()
-      const { toast } = useToast()
-      const handleStatus = async (status: any) =>{
+      const payment = row.original;
+      const { data } = useLogin();
+      const router = useRouter();
+      const { toast } = useToast();
+      const handleStatus = async (status: any) => {
         try {
-          await axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/payments/status/${payment.id}`, {status}, {
-            headers:{
-              Authorization: `Bearer ${data.token}`
-            }
-          })
-          .then((res)=>{
-            toast({
-              title: "Success",
-              description: res.data.message,
+          await axios
+            .patch(
+              `${process.env.NEXT_PUBLIC_API_URL}/payments/status/${payment.id}`,
+              { status },
+              {
+                headers: {
+                  Authorization: `Bearer ${data?.token}`,
+                },
+              },
+            )
+            .then((res) => {
+              toast({
+                title: "Success",
+                description: res.data.message,
+              });
             });
-          })
-          router.refresh()
+          router.refresh();
         } catch (error) {
-          console.log(error)
+          console.log(error);
           toast({
             variant: "destructive",
             title: "Failed",
             description: "Payment unsuccessfully updated",
           });
         }
-      }
+      };
       return (
         <>
           <div className="flex gap-4">
-            <Button variant={"outline"} className="text-green-500" onClick={()=>handleStatus("success")}>Success</Button>
-            <Button variant={"outline"} className="text-red-500" onClick={()=>handleStatus("pending")}>Pending</Button>
+            <Button
+              variant={"outline"}
+              className="text-green-500"
+              onClick={() => handleStatus("success")}
+            >
+              Success
+            </Button>
+            <Button
+              variant={"outline"}
+              className="text-red-500"
+              onClick={() => handleStatus("pending")}
+            >
+              Pending
+            </Button>
           </div>
         </>
-      )
-    }
-  }
+      );
+    },
+  },
 ];
