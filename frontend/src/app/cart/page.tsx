0953003fd4c54c7456abb7838b9c54d4303b21de
MrIsdi <isdiridho@gmail.com>
2024-03-26 10:56:54 +0700

    fix: payment

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -160,6 +160,7 @@ export default function Cart() {
       item_details.forEach((a)=>{
         a.payment_id = response.data.data.id
         delete a.url
+        delete a.id
       })
       await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/histories`,
